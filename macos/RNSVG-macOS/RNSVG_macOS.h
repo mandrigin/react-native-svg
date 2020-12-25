274cb88c9be42d1199b77f59476c864bbb075329
Igor Mandrigin <i@mandrigin.ru>
2020-12-25 15:29:08 +0100

    use macos target

---
+++ macos/RNSVG-macOS/RNSVG_macOS.h
@@ -0,0 +1,12 @@
+//
+//  RNSVG_macOS.h
+//  RNSVG-macOS
+//
+//  Created by Igor Mandrigin on 2020-12-25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNSVG_macOS : NSObject
+
+@end
---
+++ macos/RNSVG-macOS/RNSVG_macOS.m
@@ -0,0 +1,12 @@
+//
+//  RNSVG_macOS.m
+//  RNSVG-macOS
+//
+//  Created by Igor Mandrigin on 2020-12-25.
+//
+
+#import "RNSVG_macOS.h"
+
+@implementation RNSVG_macOS
+
+@end
